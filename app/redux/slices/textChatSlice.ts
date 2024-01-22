b2f388820126032c208681ef7e1fb7321bd29cf3
TechBoss731213 <kenji.nakagawa731213@gmail.com>
2024-01-22 18:47:11 +0900

    added new thread feature

--- app/redux/slices/textChatSlice.ts
+++ app/redux/slices/textChatSlice.ts
@@ -1,25 +1,45 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-interface TextChatState {
-	history: { role: string; content: string }[];
+interface Message {
+	role: string;
+	content: string;
+}
+
+interface Thread {
+	id: number;
+	messages: Message[];
 	messageCount: number;
 }
 
+interface TextChatState {
+	threads: Thread[];
+	activeThreadId: number;
+}
+
 const initialState: TextChatState = {
-	history: [],
-	messageCount: 0,
+	threads: [{ id: 1, messages: [], messageCount: 0 }],
+	activeThreadId: 1,
 };
 
 const textChatSlice = createSlice({
 	name: 'textChat',
 	initialState,
 	reducers: {
-		addMessage: (state, action: PayloadAction<{ role: string; content: string }>) => {
-			state.history.push(action.payload);
-			state.messageCount += 1;
+		addMessage: (state, action: PayloadAction<Message>) => {
+			const activeThread = state.threads.find(thread => thread.id === state.activeThreadId);
+			if (activeThread) {
+				activeThread.messages.push(action.payload);
+				activeThread.messageCount = activeThread.messages.length;
+			}
+		},
+		addThread: (state, action: PayloadAction<number>) => {
+			state.threads.push({ id: action.payload, messages: [], messageCount: 0 });
+		},
+		switchThread: (state, action: PayloadAction<number>) => {
+			state.activeThreadId = action.payload;
 		},
 	},
 });
 
-export const { addMessage } = textChatSlice.actions;
+export const { addMessage, addThread, switchThread } = textChatSlice.actions;
 export default textChatSlice.reducer;
